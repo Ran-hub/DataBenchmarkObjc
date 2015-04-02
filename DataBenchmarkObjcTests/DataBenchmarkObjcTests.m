49657751c30edd460a320578f8ec7d0f3f4c095d
Stanislav Kirichok <stanislav.kirichok@yalantis.com>
2015-04-02 10:57:30 +0300

    init

--- DataBenchmarkObjcTests/DataBenchmarkObjcTests.m
+++ DataBenchmarkObjcTests/DataBenchmarkObjcTests.m
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+static const NSUInteger iterationCount = 100000;
+static const NSString *testString = @"test";
+
 @interface DataBenchmarkObjcTests : XCTestCase
 
 @end
@@ -25,16 +28,62 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
+//MARK: Arrays
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void) testArrayWriteSpeed {
+    [self measureBlock: ^{
+        [self generateArray];
     }];
 }
+
+- (void) testArrayFastEnumReadSpeed {
+    NSMutableArray *testArray = [self generateArray];
+    
+    [self measureBlock: ^{
+        for (NSString *str in testArray) {
+            NSString *const constant = str;
+        }
+    }];
+}
+
+- (void) testArrayByIndexReadSpeed {
+    NSMutableArray *testArray = [self generateArray];
+    
+    [self measureBlock: ^{
+        for (NSUInteger i = 0; i < testArray.count; i++) {
+            NSString *const constant = testArray[i];
+        }
+    }];
+}
+
+- (void) testArrayByIndexDeleteSpeed {
+    NSMutableArray *testArray = [self generateArray];
+    
+    [self measureBlock: ^{
+        for (NSUInteger i = 0; i < testArray.count; i++) {
+            [testArray removeObjectAtIndex:i];
+        }
+    }];
+}
+
+- (void) testArrayFindSpeed {
+    NSMutableArray *testArray = [self generateArray];
+    NSString *const last = testArray.lastObject;
+    [self measureBlock: ^{
+        [testArray indexOfObject:last];
+    }];
+}
+
+#pragma mark - Helper methods
+
+ - (NSMutableArray *)generateArray {
+     NSMutableArray *testArray = [NSMutableArray new];
+    for (NSUInteger i = 0; i < iterationCount; i++) {
+        [testArray addObject:[NSString stringWithFormat:@"%@%lu", testString, (unsigned long)i]];
+    }
+    
+    return testArray;
+}
+
 
 @end
