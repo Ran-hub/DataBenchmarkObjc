73cc36a42df6da695918f8bb8d985b7a34e707dd
Stanislav Kirichok <stanislav.kirichok@yalantis.com>
2015-04-02 10:41:41 +0300

    Initial Commit

---
+++ DataBenchmarkObjc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DataBenchmarkObjc
+//
+//  Created by Stanislav on 02.04.15.
+//  Copyright (c) 2015 Stanislav. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
